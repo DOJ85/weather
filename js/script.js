51de0331c3bc46fa93556e12cdde062a5b78a81d
Unknown <florianpelloux85@gmail.com>
2018-07-13 08:49:58 +0200

    0.0.8

--- js/script.js
+++ js/script.js
@@ -48,10 +48,10 @@ function startConverting() {
 
         }else{
           interimTranscripts += transcript;
-          console.log(interimTranscripts + transcript);
         }
       }
       var r = finalTranscripts + interimTranscripts;
+      console.log(finalTranscripts + interimTranscripts);
       console.log(finalTranscripts + interimTranscripts);
       //var filtered = r.replace(/quel temps fait-il à/g, "");
       var filtered = r.replace(new RegExp ('quel temps fait-il à' || 'comment est le temps à'), "");
